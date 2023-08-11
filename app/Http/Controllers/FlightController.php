63b627d0e708ffc20459bb941e0981ebbc3628f2
Akshayhazare003 <138850336+Akshayhazare003@users.noreply.github.com>
2023-08-11 16:37:19 +1000

    3.1

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -1,10 +1,29 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
+use App\Http\Controllers\Controller;
+use App\Models\Flight;
 use Illuminate\Http\Request;
-
+ 
 class FlightController extends Controller
 {
-    //
+    /**
+     * Store a new flight in the database.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+{
+    // Validate the request...
+
+    $flight = new Flight;
+
+    $flight->name = $request->name;
+
+    $flight->save();
+
+    return response()->json(['message' => 'Flight stored successfully'], 201);
+}
 }
