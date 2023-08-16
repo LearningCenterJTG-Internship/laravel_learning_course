ed02a166f1d4056f98d27772a84183e184b26fbb
Akshayhazare003 <138850336+Akshayhazare003@users.noreply.github.com>
2023-08-16 12:28:12 +1000

    middleware

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,22 @@
+namespace App\Http\Middleware;
+ 
+use Closure;
+ 
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->input('token') !== 'my-secret-token') {
+            return redirect('home');
+        }
+ 
+        return $next($request);
+    }
+}
